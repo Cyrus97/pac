f21b69e6b24eea00163d95ef9b8759fbe00d69ae
Cyrus97 <liuxingran97@qq.com>
2019-11-28 00:10:51 +0800

    简化代码

--- generator.sh
+++ generator.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 
-cat $GITHUB_WORKSPACE/user-rule.txt
-tail -10 $GITHUB_WORKSPACE/gfwlist/gfwlist.txt
-pwd
 genpac --format=pac --pac-proxy="SOCKS5 127.0.0.1:7891" \
-                    --user-rule-from=$GITHUB_WORKSPACE/user-rule.txt \
+                    --user-rule-from=user-rule.txt \
                     --gfwlist-url=- \
-                    --gfwlist-local=$GITHUB_WORKSPACE/gfwlist/gfwlist.txt \
+                    --gfwlist-local=gfwlist/gfwlist.txt \
                     --output=7891.pac
 tail -10 7891.pac
