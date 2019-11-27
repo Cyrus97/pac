279512926ce2ccaa47ea66173283e5b58ae142e3
Cyrus97 <liuxingran97@qq.com>
2019-11-28 00:33:22 +0800

    增加

--- generator.sh
+++ generator.sh
@@ -2,5 +2,10 @@
 
 genpac --format=pac --pac-proxy="SOCKS5 127.0.0.1:7891" \
                     --user-rule-from=user-rule.txt \
+                    --gfwlist-url=- \
                     --gfwlist-local=gfwlist/gfwlist.txt \
                     --output=7891.pac
+genpac --format=pac --pac-proxy="SOCKS5 127.0.0.1:44444" \
+                    --user-rule-from=user-rule.txt \
+                    --gfwlist-local=gfwlist/gfwlist.txt \
+                    --output=44444.pac
