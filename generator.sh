3362b687c7a4a097387b665006c730e118a86e6e
Cyrus97 <liuxingran97@qq.com>
2019-11-27 23:00:21 +0800

    测试

--- generator.sh
+++ generator.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
 cat user-rule.txt
-genpac --format=pac --pac-proxy="SOCKS5 127.0.0.1:7891" --user-rule-from=user-rule.txt --output 7891.pac
+genpac --format=pac --pac-proxy="SOCKS5 127.0.0.1:7891" \
+                    --user-rule-from=user-rule.txt \
+                    --gfwlist-local=gfwlist/gfwlist.txt \
+                    --output=7891.pac
 tail -10 7891.pac
