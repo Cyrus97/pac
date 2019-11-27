93f1a0dfcce33945f38a40ca57376128680d9141
Cyrus97 <liuxingran97@qq.com>
2019-11-28 00:24:41 +0800

    使用默认 gfwlist

--- generator.sh
+++ generator.sh
@@ -2,7 +2,5 @@
 
 genpac --format=pac --pac-proxy="SOCKS5 127.0.0.1:7891" \
                     --user-rule-from=user-rule.txt \
-                    --gfwlist-url=- \
                     --gfwlist-local=gfwlist/gfwlist.txt \
                     --output=7891.pac
-tail -10 7891.pac
