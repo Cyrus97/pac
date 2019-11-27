b31d8ce06efdae5d4202ba1ab60a7c3b55e65433
Cyrus97 <liuxingran97@qq.com>
2019-11-27 23:31:15 +0800

    测试

--- generator.sh
+++ generator.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-cat user-rule.txt
+cat $GITHUB_WORKSPACE/user-rule.txt
 genpac --format=pac --pac-proxy="SOCKS5 127.0.0.1:7891" \
-                    --user-rule-from=user-rule.txt \
+                    --user-rule-from=$GITHUB_WORKSPACE/user-rule.txt \
                     --gfwlist-local=gfwlist/gfwlist.txt \
                     --output=7891.pac
 tail -10 7891.pac
