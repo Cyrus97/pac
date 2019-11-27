47bc1b54c2d6ac0bfb0a1eafe9141e3002a7249b
Cyrus97 <liuxingran97@qq.com>
2019-11-27 23:40:54 +0800

    支持使用本地 gfwlist

--- generator.sh
+++ generator.sh
@@ -4,6 +4,6 @@ cat $GITHUB_WORKSPACE/user-rule.txt
 genpac --format=pac --pac-proxy="SOCKS5 127.0.0.1:7891" \
                     --user-rule-from=$GITHUB_WORKSPACE/user-rule.txt \
                     --gfwlist-url=- \
-                    --gfwlist-local=gfwlist/gfwlist.txt \
+                    --gfwlist-local=$GITHUB_WORKSPACE/gfwlist/gfwlist.txt \
                     --output=7891.pac
 tail -10 7891.pac
