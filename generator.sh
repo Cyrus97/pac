142f54245f6f7fab1e36928b0a3d6fa554f66626
Cyrus <liuxingran97@qq.com>
2022-02-16 20:51:24 +0800

    Update generator.sh

--- generator.sh
+++ generator.sh
@@ -9,3 +9,7 @@ genpac --format=pac --pac-proxy="SOCKS5 127.0.0.1:44444" \
                     --user-rule-from=user-rule.txt \
                     --gfwlist-local=gfwlist/gfwlist.txt \
                     --output=44444.pac
+genpac --format=pac --pac-proxy="SOCKS5 192.168.1.3:7891" \
+                    --user-rule-from=user-rule.txt \
+                    --gfwlist-local=gfwlist/gfwlist.txt \
+                    --output=mi3.pac
